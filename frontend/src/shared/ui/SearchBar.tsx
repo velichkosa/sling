b774bcf8c3acc60c6be02aa4d42fc3b027798f45
velichkosa <velichko.sergey.a@gmail.com>
2025-03-23 20:27:58 +0500

    refactoring

--- frontend/src/shared/ui/SearchBar.tsx
+++ frontend/src/shared/ui/SearchBar.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
-import {ClearIcon, Dropdown, DropdownItem, Input, SearchBox, SearchContainer, SearchIcon} from './searchBarStyles'
 import {useLocation, useNavigate} from "react-router-dom";
+import styled from "styled-components";
+import {Search, X} from "lucide-react";
 
 
 const suggestions = ["React", "TypeScript", "Styled Components", "Django", "AI", "OpenAI", "GraphQL"];
@@ -71,3 +72,77 @@ const SearchBar: React.FC<{ setQuery: (q: string) => void }> = ({setQuery}) => {
 };
 
 export default SearchBar;
+
+const SearchContainer = styled.div`
+    position: relative;
+    width: 100%;
+    max-width: 600px;
+`;
+
+const SearchBox = styled.div<{ $focused: boolean, }>`
+    display: flex;
+    align-items: center;
+    background: ${({theme}) => theme.searchBox.background};
+    border: 1px solid rgba(255, 255, 255, 0.3);
+    border-radius: 24px;
+    padding: 12px 16px;
+    box-shadow: ${({$focused}) => ($focused ? "0 4px 10px rgba(255, 255, 255, 0.2)" : "none")};
+    backdrop-filter: blur(10px);
+    transition: all 0.3s ease;
+    cursor: text;
+`;
+
+const Input = styled.input`
+    flex: 1;
+    background: transparent;
+    border: none;
+    outline: none;
+    color: ${({theme}) => theme.searchBox.input};
+    font-size: 16px;
+    padding: 0 8px;
+
+    &::placeholder {
+        color: ${({theme}) => theme.searchBox.placeholder};
+    }
+`;
+
+const SearchIcon = styled(Search)`
+    color: ${({theme}) => theme.searchBox.searchIcon};
+    width: 20px;
+    height: 20px;
+`;
+
+const ClearIcon = styled(X)`
+    color: ${({theme}) => theme.searchBox.clearIcon};
+    width: 20px;
+    height: 20px;
+    cursor: pointer;
+    transition: color 0.2s ease;
+
+    &:hover {
+        color: rgba(255, 255, 255, 0.7);
+    }
+`;
+
+const Dropdown = styled.div`
+    position: absolute;
+    bottom: 48px; /* Смещаем вверх */
+    width: 100%;
+    background: ${({theme}) => theme.searchBox.dropdownBackground};
+    border-radius: 12px;
+    backdrop-filter: blur(10px);
+    overflow: hidden;
+    box-shadow: ${({theme}) => theme.searchBox.dropdownBoxShadow};
+`;
+
+
+const DropdownItem = styled.div`
+    padding: 12px 16px;
+    color: ${({theme}) => theme.searchBox.dropdownItem};
+    cursor: pointer;
+    transition: background 0.2s ease;
+
+    &:hover {
+        background: rgba(255, 255, 255, 0.2);
+    }
+`;
