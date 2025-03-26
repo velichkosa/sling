9f940a45af4f53e0606d590b6aec4baddfb7e0cf
Sergey Velichko <velichko.sergey.a@gmail.com>
2025-03-26 15:16:15 +0500

    deploy

--- backend/images/models.py
+++ backend/images/models.py
@@ -5,7 +5,7 @@ from dict.models import Tag, FormFactor, WorkType, Slings
 
 
 class Image(BaseUuidMixin, BaseTimestampedMixin, models.Model):
-    title = models.CharField(verbose_name='Название изображения', max_length=255)
+    title = models.CharField(verbose_name='Название изображения', unique=True, max_length=255)
     image = models.ImageField(verbose_name='Ссылка на изображение', upload_to='images/sling_schemes/')
     description = models.TextField(verbose_name='Описание изображения', **nb_none)
     tags = models.ManyToManyField(Tag, verbose_name='Тэги', related_name='images', blank=True)
